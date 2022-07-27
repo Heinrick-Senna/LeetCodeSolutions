1b1b8fcad9a1c856e9d3126965bf0c4e92d7e84b
Marcelo Heinrick Romeiro Senna <marcelohz198@hotmail.com>
2022-07-27 00:26:46 -0300

    Time: 240 ms (13.80%), Space: 42.6 MB (51.46%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++ ) {
+        let base = nums[i];
+        for (let z = 0; z < nums.length; z++) {
+            let sum = nums[z]
+            if (base + sum == target && i != z) return [i, z];
+        }
+    }
+};
