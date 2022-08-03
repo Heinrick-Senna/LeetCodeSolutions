608316d50bb0dbddb460d6b2a1d431e4438b8bab
Marcelo Heinrick Romeiro Senna <marcelohz198@hotmail.com>
2022-08-03 10:52:40 -0300

    Time: 123 ms (50.00%), Space: 45 MB (92.15%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.js
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.js
@@ -3,8 +3,5 @@
  * @return {number[]}
  */
 var buildArray = function(nums) {
-    let array = nums.map((n, i) => {
-        return nums[n];
-    })
-    return array;
+    return nums.map(n => nums[n])
 };
