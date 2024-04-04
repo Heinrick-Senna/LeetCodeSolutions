2d3daea0653372b62e7780e3b3474506d1fc9cf1
Marcelo Heinrick Romeiro Senna <marcelohz198@hotmail.com>
2024-04-04 12:38:15 -0300

    Delete 1-two-sum directory

--- 1-two-sum/1-two-sum.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-var twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length; i++ ) for (let z = 0; z < nums.length; z++) if (nums[i] + nums[z] == target && i != z) return [i, z];
-};
