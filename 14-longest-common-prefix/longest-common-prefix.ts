b9c3e985ea478b14f329edaa933df7a3cc59db20
Marcelo Heinrick Romeiro Senna <marcelohsenna@protonmail.com>
2026-05-13 10:44:27 -0300

    Time: 160 ms (5.20%) | Memory: 64.7 MB (5.14%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.ts
+++ 14-longest-common-prefix/longest-common-prefix.ts
@@ -1,5 +1,6 @@
 function longestCommonPrefix(strs: string[]): string {
     let prefix = strs[0];
+    let pointer = prefix.length - 1;
 
     for (let z = 1; z < strs.length; z++) {
         const str = strs[z];
@@ -9,23 +10,35 @@ function longestCommonPrefix(strs: string[]): string {
             break;
         }
 
-        for (let i = 0; i < Math.max(str.length, prefix.length); i++) {
-            const char = prefix[i];
-            const newChar = str[i];
-
-            console.log(i, char, newChar);
-
+        for (let s = pointer; s >= 0; s--) {
+            let char = prefix[s];
+            let newChar = str[s];
+            console.log(char, newChar, s)
             if (char != newChar) {
-                prefix = str.slice(0, i);
-                break;
+                console.log('teste')
+                pointer = s-1;
             }
         }
+
+        console.log(pointer)
+
+        // while (char != newChar) {
+        //     console.log(char, newChar, pointer)
+
+        //     pointer--;
+
+        //     char = prefix[pointer];
+        //     newChar = str[pointer];            
+        // }
+
+        prefix = str.slice(0, pointer+1);
+        console.log('String atual: ', str)
+        console.log('Prefix atual: ', prefix)
+        console.log('Pointer atual: ', pointer)
 
         if (prefix == '') {
             break;
         }
-
-
     }
 
     return prefix
