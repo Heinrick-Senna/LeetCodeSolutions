15ab82b5e095714d7ff8c0ace5bf7ac66d665288
Marcelo Heinrick Romeiro Senna <marcelohsenna@protonmail.com>
2026-05-14 11:44:26 -0300

    Time: 0 ms (100.00%) | Memory: 55.7 MB (37.93%) - LeetSync

--- 343-integer-break/integer-break.ts
+++ 343-integer-break/integer-break.ts
@@ -2,39 +2,16 @@ function integerBreak(n: number): number {
     if (n == 3) return 2;
 
     const returnRest = (amount: number): number[] => {
+        if (amount == 2) return [1]
+        if (amount == 3) return [3]
+
         const rest = amount % 3;
-        console.log(amount, rest);
 
-        if (amount == 2) {
-            return [1]
-        }
-
-        if (amount == 3) {
-            return [3]
-        }
-
-        if (rest == 0) {
-            return Array(amount / 3).fill(3);
-        }
-
-        if (rest == 1) {
-            const newAmount = amount - 4;
-            return [2, 2, ...(returnRest(newAmount))];
-        }
-
-        if (rest == 2) {
-            return [2, ...(returnRest(amount-2))]
-        }
+        if (rest == 0) return Array(amount / 3).fill(3);
+        if (rest == 1) return [2, 2, ...(returnRest(amount - 4))];
+        if (rest == 2) return [2, ...(returnRest(amount-2))]
     }
 
     const sum = returnRest(n);
-
-    console.log(sum);
-    let num = 1;
-
-    sum.forEach(x => {
-        num = x * num;
-    })
-
-    return num
+    return sum.reduce((acc, cv) => acc * cv);
 };
