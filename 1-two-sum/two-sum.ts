3dba53e2221a20603da8c6fff48d3832d1992111
Marcelo Heinrick Romeiro Senna <marcelohsenna@protonmail.com>
2026-05-14 14:53:25 -0300

    Time: 6 ms (39.10%) | Memory: 57.2 MB (46.10%) - LeetSync

---
+++ 1-two-sum/two-sum.ts
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (typeof map.get(complement) == 'number') {
+            console.log(map)
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    return null;
+};
