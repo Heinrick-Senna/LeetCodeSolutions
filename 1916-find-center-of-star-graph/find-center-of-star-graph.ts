5beb38fe71d63a93bf646f9dfbf4ea6ba5c2d8f1
Marcelo Heinrick Romeiro Senna <marcelohsenna@protonmail.com>
2026-05-28 14:40:50 -0300

    Time: 0 ms (100.00%) | Memory: 76.7 MB (69.49%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.ts
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.ts
@@ -1,8 +1,5 @@
 function findCenter(edges: number[][]): number {
-    const graph: Map<number, null> = new Map()
-    graph.set(edges[0][0], null);
-    graph.set(edges[0][1], null);
 
     const val1 = edges[1][0];
-    return graph.has(val1) ? val1 : edges[1][1];
+    return edges[0].some(item => item == val1) ? val1 : edges[1][1];
 };
