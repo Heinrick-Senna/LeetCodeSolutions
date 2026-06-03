09628c81a159addc859e058b1081e59030660840
Marcelo Heinrick Romeiro Senna <marcelohsenna@protonmail.com>
2026-06-03 10:31:50 -0300

    Time: 5 ms (40.00%) | Memory: 62.5 MB (20.00%) - LeetSync

--- 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.ts
+++ 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.ts
@@ -1,24 +1,11 @@
 function countSubstrings(s: string, c: string): number {
-    // const substringPoints = new Map<number, number>();
-
     let count = 0;
     let pointer = 0;
-    for (let i = 0; i < s.length; i++) {
-        const char = s[i];
-
+    for (const char of s) {
         if (char == c) {
             pointer++;
-            count+= pointer;
-            // substringPoints.set(i, pointer);
+            count += pointer;
         }
-    };
-
-    // let count = 0;
-    // for (let i = 0; i < s.length; i++) {
-    //     const char = s[i];
-
-    //     if (char == c) count+= substringPoints.get(i);
-    // };
-
+    }
     return count
 };
